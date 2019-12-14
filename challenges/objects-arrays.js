bce4cdfcfb8a333f8783a7ba72b864b7bf355c04
IsaiahFlax <flax.isaiah@gmail.com>
2019-12-13 21:12:36 -0700

    Update objects-arrays.js
    finished this section

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -170,7 +170,7 @@ console.log(populationTotal);
 
 /*
 
-Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+STRETCH: If you haven't already, convert your array method callbacks into arrow functions.
 
 */
 
